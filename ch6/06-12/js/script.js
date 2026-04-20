9c3ddc09929949a07aaa527a23d3ebda0ef6d6cd
itsch <itsch@example.com>
2026-04-20 11:49:18 +0900

    ch7-4

---
+++ ch6/06-12/js/script.js
@@ -0,0 +1,16 @@
+const items = document.querySelectorAll('.img-item');
+
+for (let i = 0; i <items.length; i++) {
+    const keyframes = {
+        opacity: [0, 1],
+        rotate: ['5deg', 0],
+        scale: [1.1, 1],
+        translate: ['0 50px', 0],
+    };
+    const options = {
+        duration: 600,
+        delay: i * 300,
+        fill: 'forwards',
+    };
+    items[i].animate(keyframes, options);
+}
